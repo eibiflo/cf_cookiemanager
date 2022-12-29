e91dea30cf0f726c314e86da8ec7188b8e30bb2a
eibiflo <eibiflo4@gmail.com>
2022-12-29 21:38:12 +0100

    [TASK] Added Icons

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -3,6 +3,6 @@
 return [
     'cf_cookiemanager-plugin-cookiefrontend' => [
         'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        'source' => 'EXT:cf_cookiemanager/Resources/Public/Icons/user_plugin_cookiefrontend.svg'
+        'source' => 'EXT:cf_cookiemanager/Resources/Public/Icons/Extension.svg'
     ],
 ];
